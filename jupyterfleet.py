57c276d8208f92473112c445c16c62b4a9a62208
andrew-leith <andrew-leith@users.noreply.github.com>
2018-03-30 16:16:32 -0400

    see changelog

--- jupyterfleet.py
+++ jupyterfleet.py
@@ -14,55 +14,110 @@ class cloud:
 		self.platform = platform
 
 	def createTable(self):
+		"""Create a textfile with pastable links to the Jupyter instances in the form 'ip:port'
+
+		Uses file(s):
+			'ips_newline.txt'
+
+		Creates file(s):
+			'ips_newline_port.txt'
+
+		Removes file(s):
+			'ips.txt'
+
+		"""
 		awkCommand = 'awk \'$0=$0":' + str(yamlPar["instance-creation"]["instance-configuration"]["jupyter-port"]) + '"\' ' + "ips_newline.txt" + ' > ips_newline_port.txt'
-		# assemble the command to append the Jupyter port to the IPs
 		subprocess.call([awkCommand], shell=True)
 		subprocess.call(['rm ips.txt'], shell=True)
-		# remove intermediary files
 
 	def createDirectory(self):
+		"""Create an HTML user directory matching each user to an IP and displaying any extra instances as 'unassigned'
+
+		This function will abort if there are not enough instances for all participants, as there is no way to predict user priority
+
+		This HTML file can then be pushed directly to any website and will let users access their instances by clicking a link
+
+		#TODO: MAYBE ADD AN OPTION TO PUT THE JUPYTER SERVER PASSWORD AT THE TOP OF THE FILE (AND CORRESPONDING YAML FIELD)
+
+		Uses file(s):
+			The 'user-list' file denoted in the YAML
+			'generate_direcrtory.sh'
+			'ips_newline_port.txt'
+
+		Creates file(s):
+			'user_directory.html'
+
+		Removes file(s):
+			N/A
+			
+		"""
 		if yamlPar["instance-creation"]["instance-configuration"]["directory-parameters"]["generate-directory"] == True:
 			num_lines = sum(1 for line in open(yamlPar["instance-creation"]["instance-configuration"]["directory-parameters"]["user-list"]))
 			if num_lines > yamlPar["instance-creation"]["cli-parameters"]["instance-number"]:
 				sys.exit("\033[1m" + "Warning: Insufficient instances for all participants.  Exiting without generating directory." + "\033[0m")
 			subprocess.call(['bash generate_directory.sh > user_directory.html'], shell=True)
-			# call the script that builds the html directory
-
 
 
 
 class aws(cloud):
 
 	def checkSoftware(self):
+		"""Check to see if the AWS CLI tool (the only current software dependency) is in the user's PATH
+
+		Execution will be terminated if it is not found, as it is needed for this software to interact with AWS
+
+		"""
 		missingAWS = subprocess.call(['which aws'], shell=True, stdout=subprocess.PIPE)
-		# this will resolve to 0 if the AWS CLI is installed
 		if missingAWS == True:
 			sys.exit("\033[1m" + "Error: The AWS Command Line Interface is not installed or not in your PATH.  Please refer to the AWS documentation for the installation instructions for your system." + "\033[0m")
 
 
 	def configureCLI(self):
+		"""Configure the AWS CLI using the parameters specified in the YAML'
+
+		Existing configuration files will be overwritten using the YAML's parameters if this function is called
+
+		Uses file(s):
+			None
+
+		Creates file(s):
+			~/.aws/config
+			~/.aws/credentials
+
+		Removes file(s):
+			None
+			
+		"""
 		if yamlPar["instance-creation"]["aws-credentials"]["generate-configuration"] == True:
-			subprocess.call(['echo "[default]" > ~/.aws/testconfig'], shell=True)
-			textCommand = 'echo "region = ' + yamlPar["instance-creation"]["aws-credentials"]["default-region"] + ' " >> ~/.aws/testconfig'
+			subprocess.call(['echo "[default]" > ~/.aws/config'], shell=True)
+			textCommand = 'echo "region = ' + yamlPar["instance-creation"]["aws-credentials"]["default-region"] + ' " >> ~/.aws/config'
 			subprocess.call([textCommand], shell=True)
 			# write the two-line 'config' file setting the default region to be used throughout
 
-			subprocess.call(['echo "[default]" > ~/.aws/testcredentials'], shell=True)
-			textCommand = 'echo "aws_access_key_id = ' + yamlPar["instance-creation"]["aws-credentials"]["key-id"] + ' " >> ~/.aws/testcredentials'
+			subprocess.call(['echo "[default]" > ~/.aws/credentials'], shell=True)
+			textCommand = 'echo "aws_access_key_id = ' + yamlPar["instance-creation"]["aws-credentials"]["key-id"] + ' " >> ~/.aws/credentials'
 			subprocess.call([textCommand], shell=True)
-			textCommand = 'echo "aws_secret_access_key = ' + yamlPar["instance-creation"]["aws-credentials"]["secret-key"] + ' " >> ~/.aws/testcredentials'
+			textCommand = 'echo "aws_secret_access_key = ' + yamlPar["instance-creation"]["aws-credentials"]["secret-key"] + ' " >> ~/.aws/credentials'
 			subprocess.call([textCommand], shell=True)
 
 	def checkConfig(self):
+		"""Check to see if the CLI is configured (i.e. if the last step worked)
+
+		If this function causes execution to break, the configuration files were not created, likely due to a permissions issue
+
+		"""
 		missingConf = subprocess.call(['aws configure get aws_access_key_id'], shell=True, stdout=subprocess.PIPE)
 		# this will resolve to 0 if there is a key in the configuration file
 		if missingConf == True:
 			sys.exit("\033[1m" + "AWS CLI is not configured.  Please ensure the YAML file includes your AWS credentials." + "\033[0m")
-			# be sure that the CLI is configured, either through the above block or through previous settings
 
 	def requestInstances(self):
+		"""Request on-demand EC2 instances of the type specified in the YAML
+
+		Once the instances are requested, excecution will pause for 3 minutes so they can fully come online
+
+		"""
 		instanceCreate = 'aws ec2 run-instances --image-id ' + yamlPar["instance-creation"]["cli-parameters"]["ami-id"] + ' --count ' + str(yamlPar["instance-creation"]["cli-parameters"]["instance-number"]) + ' --instance-type ' + yamlPar["instance-creation"]["cli-parameters"]["instance-type"] + ' --key-name ' + yamlPar["instance-creation"]["cli-parameters"]["keyfile"]["key-name"] + ' --security-group-ids ' + yamlPar["instance-creation"]["cli-parameters"]["security-group-id"]
-		# generate the command to instantiate the instances in accordance with YAML specifications
 		subprocess.call([instanceCreate], shell=True, stdout=subprocess.PIPE)
 		print "Instances have been successfully requested and are presently instantiating."
 		print "Execution will now pause for 3 minutes to be sure all instances are active."
@@ -70,6 +125,11 @@ class aws(cloud):
 		time.sleep(180)
 
 	def spotRequest(self):
+		"""Request spot EC2 instances of the type specified in the YAML
+
+		TODO: ADD DOCUMENTATION FOR FALLBACK PROCEDURES ONCE THEY ACTUALLY EXIST
+
+		"""
 		instanceCreate = 'aws ec2 request-spot-instances --spot-price 0.007 --instance-count ' + str(yamlPar["instance-creation"]["cli-parameters"]["instance-number"]) + ' --launch-specification \"{\\\"KeyName\\\": \\\"' + yamlPar["instance-creation"]["cli-parameters"]["keyfile"]["key-name"] + '\\\", \\\"ImageId\\\": \\\"' + yamlPar["instance-creation"]["cli-parameters"]["ami-id"] + '\\\", \\\"InstanceType\\\": \\\"' + yamlPar["instance-creation"]["cli-parameters"]["instance-type"] + '\\\", \\\"SecurityGroupIds\\\": [\\\"' + yamlPar["instance-creation"]["cli-parameters"]["security-group-id"] + '\\\"]}\"'
 		# generate the command to instantiate the instances in accordance with YAML specifications
 		subprocess.call([instanceCreate], shell=True, stdout=subprocess.PIPE)
@@ -77,8 +137,16 @@ class aws(cloud):
 		print "Execution will now pause for 3 minutes to be sure all instances are active."
 		print "Their status can be viewed in the web console at this point."
 		time.sleep(180)
+		if yamlPar["instance-creation"]["cli-parameters"]["spot"]["fallback"] == True:
+			lengthBase = int(yamlPar["instance-creation"]["cli-parameters"]["instance-number"])
+			lengthCheck = int(subprocess.check_output(['aws ec2 describe-spot-instance-requests --filters Name=state,Values=active | grep "Your spot request is fulfilled." | wc -l'], shell=True))
+			allActive = (lengthBase == lengthCheck)
+			# check if all requested spot instances have actually been generated (i.e. see if the number of spot requests marked 'active' is the same as the number requested)
 
 	def kill(self):
+		"""Kill all instances in the default region"""
+
+		# TODO: MAKE THIS FUNCTION MORE CONTROLLABLE, WITH TEXT VALIDATION INSTEAD OF WAITING, EXITING WITHOUT KILLING OTHERWISE
 		if arguments.kill:
 			print "All instances in the region specified in the YAML file will now be deactivated.  Execution will pause for 30 seconds to give you a chance to cancel (ctrl+c) if that is not desired.  Once that time elapses, this operation is irreversible."
 			time.sleep(30)
@@ -86,29 +154,53 @@ class aws(cloud):
 			sys.exit("Resources are now being deactivated.  Execution will now terminate.  Spin-down status can be monitored from the console and will take approximately one minute.")
 
 	def getIPs(self):
+		"""Retrieve the IPs for each instance and store them in a file, with each IP on a separate line
+
+		IPs will be retrieved for all active instances in the default region, including any previously extant
+
+		Uses file(s):
+			None
+
+		Creates file(s):
+			'ips.txt'
+			'ips_newline.txt'
+
+		Removes file(s):
+			None
+			
+		"""
 		subprocess.call(['aws ec2 describe-instances   --query "Reservations[*].Instances[*].PublicIpAddress"   --output=text > ips.txt'], shell=True)
-		# use the AWS CLI to retrieve the list of IPs in the default region
 		subprocess.call(['tr "\t" "\n" < ips.txt > ips_newline.txt'], shell=True)
 		# split the raw output onto separate lines
 
 	def manageKey(self):
-		keyLocation = yamlPar["instance-creation"]["cli-parameters"]["keyfile"]["key-path"] + '/' + yamlPar["instance-creation"]["cli-parameters"]["keyfile"]["key-name"] + ".pem"
+		"""Process the AWS keyfile and be sure it is usable
+
+		This function checks the permissions of the keyfile and sees if they are '400' (user read only) as required by AWS
+
+		If the permissions are wrong, it attempts to change them to be correct
+
+		Finally, it verifies the change worked - if not, it aborts, as the instances will not be accessible
+
+		Soon, a flag '--skip' will allow the user to skip right back here in the event that occurs
+
+		"""
+		#keyLocation = yamlPar["instance-creation"]["cli-parameters"]["keyfile"]["key-path"] + '/' + yamlPar["instance-creation"]["cli-parameters"]["keyfile"]["key-name"] + ".pem"
 		# store the full filepath of the keyfile for convenience due to length of expression
 		print("Making sure the keyfile's permissions are correctly set...")
 		if yamlPar["instance-creation"]["user-platform"] == "osx":
 			keyPermissions = subprocess.check_output(['stat -f \'%A %a %N\' ' + keyLocation], shell=True)
 		elif yamlPar["instance-creation"]["user-platform"] == "linux":
 			keyPermissions = subprocess.check_output(['stat -c \'%a %n\' ' + keyLocation], shell=True)
-		# check permissions of the keyfile
+		# check permissions of the keyfile using system-specific syntax
 		if int(keyPermissions[:3]) != 400:
-			# keyfile must have these exact permissions or the connection will be declined by AWS
 			subprocess.call(['chmod 400 ' + keyLocation], shell=True)
 			print("Attempting to change permissions to read-only...")
 			if yamlPar["instance-creation"]["user-platform"] == "osx":
 				keyPermissions = subprocess.check_output(['stat -f \'%A %a %N\' ' + keyLocation], shell=True)
 			elif yamlPar["instance-creation"]["user-platform"] == "linux":
 				keyPermissions = subprocess.check_output(['stat -c \'%a %n\' ' + keyLocation], shell=True)
-		# check again to see if update was successful with syntax based on user platform
+		# check again to see if update was successful using the same syntax
 		if int(keyPermissions[:3]) != 400:
 			sys.exit("Error: Key permissions incorrect even after an attempt to modify them, likely due to inadequate user access level.  Unable to proceed.  Please contact your system administrator for assistance.")
 		else:
@@ -116,6 +208,13 @@ class aws(cloud):
 		print("Done verifying permissions.")
 
 	def startJupyter(self):
+		"""ssh into each instance and initialize the Jupyter server
+
+		This function loops over each IP in the 'ips_newline.txt' file to generate the activation command
+
+		Jupyter is run in a detached screen to ensure it persists indefinitely
+
+		"""
 		print("Deploying Jupyter...")
 		ipFile = open("ips_newline.txt",'r')
 		for ip in ipFile:
@@ -127,7 +226,7 @@ class aws(cloud):
 				# if verbose, print the IP so user can track the process
 			bashCommand = 'ssh -oStrictHostKeyChecking=no -o \"UserKnownHostsFile /dev/null\" -i ' + keyLocation + ' ' + yamlPar["instance-creation"]["instance-configuration"]["username"] + '@' + ip + ' "screen -dm bash -c \''+ yamlPar["instance-creation"]["instance-configuration"]["conda-path"] +'/jupyter notebook\'"'
 			# assemble the command to access each IP and activate Jupyter in a detached screen
-			# do not add to UserKnownHostsFile because eventually an IP will be reused and your computer will erroneously flag it as a man-in-the-middle attack
+			# do not add to UserKnownHostsFile because eventually an IP will be reused and your computer will erroneously flag it as a man-in-the-middle attack and break execution
 			subprocess.call([bashCommand], shell=True)
 			if yamlPar["instance-creation"]["instance-configuration"]["logging"]["wait"] == True:
 				print("Waiting 120 seconds to be sure the screen will persist...")
@@ -140,6 +239,26 @@ class aws(cloud):
 		print("Jupyter should now be active on all instances.  It is recommended to spot-check a few to validate this fact.")
 
 	def createLogger(self):
+		"""Configure instance logging that will periodically check screen status on each instance
+
+		This function creates a bash script based on the YAML parameters, as cron needs a script to properly execute long looping commands
+
+		The resulting cron job will list all active screens on all instances every X minutes as specified in the YAML
+
+		TODO: Add a way to summarize this output to be reviewed in one glance without reading the long log file	
+
+		Uses file(s):
+			None
+
+		Creates file(s):
+			crontab file (if not extant)
+			'screen_check.sh'
+			'cron_screen_log.txt'
+
+		Removes file(s):
+			None
+
+		"""
 		if yamlPar["instance-creation"]["instance-configuration"]["logging"]["cron-interval"] > 0:
 		# setting the interval to 0 will disable logging
 			subprocess.call(['echo "#!/bin/bash" > screen_check.sh'], shell=True)
@@ -172,11 +291,13 @@ class aws(cloud):
 
 
 
+#'wait' - use a while loop to check every 2 minutes
 
 
-
-
-
+#Fallback behavior (what to do if requests are not filled, or if 'wait' has run unsuccessfully for 10 minutes:
+#'on-demand' - terminate unfilled requests and use on-demand instances to fill the rest of the slots
+#'ignore' - proceed without all slots filled
+#'abort' - spin-down instances and exit
 
 
 
@@ -187,9 +308,11 @@ class aws(cloud):
 parser = argparse.ArgumentParser(description='Uses a YAML file to deploy a designated number of Jupyter instances on AWS according to user specifications.  Please visit http://placeholder for a thorough explanation of the YAML file\'s format.')
 parser.add_argument('-y', '--yaml', type=str, required = True, help = 'The YAML configuraton file')
 parser.add_argument('--kill', help = 'Deactivates all resources', action="store_true")
+# TODO: add '--skip', default False, to skip directly to manageKey()
 
 # Parse arguments
 arguments = parser.parse_args()
+
 
 # Begin parsing of YAML
 yamlPar = yaml.load(open(arguments.yaml))
@@ -214,7 +337,6 @@ run.startJupyter()
 run.createLogger()
 run.createTable()
 run.createDirectory()
-# TODO: ADD MANUAL CONFIRMATION ASKING WHETHER OR NOT TO PROCEED IF NUMBER OF REGISTRANTS IS GREATER THAN THE NUMBER OF INSTANCES
 
 
 
